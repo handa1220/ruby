309af6b6503a0a016168f5fb1c7af4a172bdc37b
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-07-11 06:35:22 +0000

    [Finish]ruby-8

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -4,12 +4,7 @@ puts "何回計算を繰り返しますか？"
 times = gets.to_i
 x = 1
 
-while times >= 0 do
-  if times == 0
-    puts "計算を終了します"
-    break
-  end
-
+while x <= times do
   puts "#{x}回目の計算"
   puts "2つの値を入力してください"
 
@@ -18,15 +13,15 @@ while times >= 0 do
 
   puts "a=#{a}"
   puts "b=#{b}"
-  puts "計算結果を出力します"
 
+  puts "計算結果を出力します"
   puts "a+b=#{a+b}"
   puts "a-b=#{a-b}"
   puts "a*b=#{a*b}"
   puts "a/b=#{a/b}"
 
   x += 1
-  times -= 1
 
 end
 
+puts "計算を終了します"
